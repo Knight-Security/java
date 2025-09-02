12a3ed985827470bc5d254e4fc5fe1dc35fa03ec
knight-Security <paliwaladitya24@gmail.com>
2025-09-03 00:28:33 +0530

    number guessing

--- number_guessinggame.java
+++ number_guessinggame.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class number_guessinggame{
+public class day7{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
@@ -9,18 +9,24 @@ public class number_guessinggame{
         System.out.print("Choose end range:- ");
         int end = sc.nextInt();
         int start = 1;
+        int computer_input = rd.nextInt(first , end);
         while(start ==1){
             System.out.printf("Please choose a number between %d and %d\n",first,end);
             int user_input = sc.nextInt();
-            int computer_input = rd.nextInt(first , end);
             if (user_input == computer_input){
                 System.out.println("YAY!! YOU GUESSED RIGHT 🎊");
-            }
-            else{
-                System.out.println("YOU LOOSE COMPUTER WIN 😂");
                 System.out.println("Press 1 to continue and 0 to exit");
                 System.out.print("Choose:- ");
                 start  =sc.nextInt();
+            }
+            else if (user_input>computer_input){
+                System.out.println("Too High");
+            }
+            else if(user_input<computer_input){
+                System.out.println("Too Low");
+            }
+            else{
+                System.out.println("YOU LOOSE COMPUTER WIN 😂");
             }
         }
     }
